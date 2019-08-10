fb212689d6685a4fe8f72bf9235d15a2282a4c13
alessanderleite <alessander.logistica@gmail.com>
2019-08-09 23:41:06 -0300

    update BookCategoryService interface

--- src/main/java/br/com/alessanderleite/app/service/BookCategoryService.java
+++ src/main/java/br/com/alessanderleite/app/service/BookCategoryService.java
@@ -1,5 +1,6 @@
 package br.com.alessanderleite.app.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,8 +9,8 @@ import br.com.alessanderleite.app.model.BookCategory;
 public interface BookCategoryService {
 
 	List<BookCategory> findAll();
-	Optional<BookCategory> findById(Long id);
+	Optional<BookCategory> findById(Long id) throws IOException;
 	BookCategory create(BookCategory bookCategory);
 	void update(BookCategory bookCategory);
-	void deleteById(Long id);
+	void deleteById(Long id) throws IOException;
 }
